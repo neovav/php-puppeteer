536f9cf8b6feffd33776f5c338137b95bfe24b2f
Neovav <neovav@outlook.com>
2020-11-18 17:57:45 +0200

    Add class with constants - DevicesViewPorts

---
+++ src/Constants/DevicesViewPorts.php
@@ -0,0 +1,575 @@
+<?php
+namespace Puppeteer\Constants;
+
+class DevicesViewPorts
+{
+    const BLACKBERRY_PLAYBOOK = [
+        'width' => 600,
+        'height' => 1024,
+        'deviceScaleFactor' => 1,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const BLACKBERRY_PLAYBOOK_LANDSCAPE = [
+        'width' => 1024,
+        'height' => 600,
+        'deviceScaleFactor' => 1,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const BLACKBERRY_Z30 = [
+        'width' => 360,
+        'height' => 640,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const BLACKBERRY_Z30_LANDSCAPE = [
+        'width' => 640,
+        'height' => 360,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const GALAXY_NOTE_3 = [
+        'width' => 360,
+        'height' => 640,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const GALAXY_NOTE_3_LANDSCAPE = [
+        'width' => 640,
+        'height' => 360,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const GALAXY_NOTE_II = [
+        'width' => 360,
+        'height' => 640,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const GALAXY_NOTE_II_LANDSCAPE = [
+        'width' => 640,
+        'height' => 360,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const GALAXY_S_III = [
+        'width' => 360,
+        'height' => 640,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const GALAXY_S_III_LANDSCAPE = [
+        'width' => 640,
+        'height' => 360,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const GALAXY_S5 = [
+        'width' => 360,
+        'height' => 640,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const GALAXY_S5_LANDSCAPE = [
+        'width' => 640,
+        'height' => 360,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPAD = [
+        'width' => 768,
+        'height' => 1024,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPAD_LANDSCAPE = [
+        'width' => 1024,
+        'height' => 768,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPAD_MINI = [
+        'width' => 768,
+        'height' => 1024,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPAD_MINI_LANDSCAPE = [
+        'width' => 768,
+        'height' => 1024,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPAD_PRO = [
+        'width' => 1024,
+        'height' => 1366,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPAD_PRO_LANDSCAPE = [
+        'width' => 1366,
+        'height' => 1024,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_4 = [
+        'width' => 320,
+        'height' => 480,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_4_LANDSCAPE = [
+        'width' => 480,
+        'height' => 320,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_5 = [
+        'width' => 320,
+        'height' => 568,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_5_LANDSCAPE = [
+        'width' => 568,
+        'height' => 320,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_6 = [
+        'width' => 375,
+        'height' => 667,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_6_LANDSCAPE = [
+        'width' => 667,
+        'height' => 375,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_6_PLUS = [
+        'width' => 414,
+        'height' => 736,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_6_PLUS_LANDSCAPE = [
+        'width' => 736,
+        'height' => 414,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_7 = [
+        'width' => 375,
+        'height' => 667,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_7_LANDSCAPE = [
+        'width' => 375,
+        'height' => 667,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_7_PLUS = [
+        'width' => 414,
+        'height' => 736,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_7_PLUS_LANDSCAPE = [
+        'width' => 736,
+        'height' => 414,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_8 = [
+        'width' => 375,
+        'height' => 667,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_8_LANDSCAPE = [
+        'width' => 667,
+        'height' => 375,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_8_PLUS = [
+        'width' => 414,
+        'height' => 736,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_8_PLUS_LANDSCAPE = [
+        'width' => 736,
+        'height' => 414,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_SE = [
+        'width' => 320,
+        'height' => 568,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_SE_LANDSCAPE = [
+        'width' => 568,
+        'height' => 320,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_X = [
+        'width' => 375,
+        'height' => 812,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_X_LANDSCAPE = [
+        'width' => 812,
+        'height' => 375,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const IPHONE_XR = [
+        'width' => 414,
+        'height' => 896,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const IPHONE_XR_LANDSCAPE = [
+        'width' => 896,
+        'height' => 414,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const JIOPHONE_2 = [
+        'width' => 240,
+        'height' => 320,
+        'deviceScaleFactor' => 1,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const JIOPHONE_2_LANDSCAPE = [
+        'width' => 320,
+        'height' => 240,
+        'deviceScaleFactor' => 1,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const KINDLE_FIRE_HDX = [
+        'width' => 800,
+        'height' => 1280,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const KINDLE_FIRE_HDX_LANDSCAPE = [
+        'width' => 1280,
+        'height' => 800,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const LG_OPTIMUS_L70 = [
+        'width' => 384,
+        'height' => 640,
+        'deviceScaleFactor' => 1.25,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const LG_OPTIMUS_L70_LANDSCAPE = [
+        'width' => 640,
+        'height' => 384,
+        'deviceScaleFactor' => 1.25,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const MICROSOFT_LUMIA_550 = [
+        'width' => 640,
+        'height' => 360,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const MICROSOFT_LUMIA_950 = [
+        'width' => 360,
+        'height' => 640,
+        'deviceScaleFactor' => 4,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const MICROSOFT_LUMIA_950_LANDSCAPE = [
+        'width' => 640,
+        'height' => 360,
+        'deviceScaleFactor' => 4,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const NEXUS_10 = [
+        'width' => 800,
+        'height' => 1280,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NEXUS_10_LANDSCAPE = [
+        'width' => 1280,
+        'height' => 800,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const NEXUS_4 = [
+        'width' => 384,
+        'height' => 640,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NEXUS_4_LANDSCAPE = [
+        'width' => 640,
+        'height' => 384,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const NEXUS_5 = [
+        'width' => 360,
+        'height' => 640,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NEXUS_5_LANDSCAPE = [
+        'width' => 640,
+        'height' => 360,
+        'deviceScaleFactor' => 3,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const NEXUS_5X = [
+        'width' => 412,
+        'height' => 732,
+        'deviceScaleFactor' => 2.625,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NEXUS_5X_LANDSCAPE = [
+        'width' => 732,
+        'height' => 412,
+        'deviceScaleFactor' => 2.625,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const NEXUS_6 = [
+        'width' => 412,
+        'height' => 732,
+        'deviceScaleFactor' => 3.5,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NEXUS_6_LANDSCAPE = [
+        'width' => 732,
+        'height' => 412,
+        'deviceScaleFactor' => 3.5,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const NEXUS_6P = [
+        'width' => 412,
+        'height' => 732,
+        'deviceScaleFactor' => 3.5,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NEXUS_6P_LANDSCAPE = [
+        'width' => 732,
+        'height' => 412,
+        'deviceScaleFactor' => 3.5,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NEXUS_7 = [
+        'width' => 600,
+        'height' => 960,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NEXUS_7_LANDSCAPE = [
+        'width' => 960,
+        'height' => 600,
+        'deviceScaleFactor' => 2,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const NOKIA_LUMIA_520 = [
+        'width' => 320,
+        'height' => 533,
+        'deviceScaleFactor' => 1.5,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NOKIA_LUMIA_520_LANDSCAPE = [
+        'width' => 533,
+        'height' => 320,
+        'deviceScaleFactor' => 1.5,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const NOKIA_N9 = [
+        'width' => 480,
+        'height' => 854,
+        'deviceScaleFactor' => 1,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const NOKIA_N9_LANDSCAPE = [
+        'width' => 854,
+        'height' => 480,
+        'deviceScaleFactor' => 1,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+    const PIXEL_2 = [
+        'width' => 411,
+        'height' => 731,
+        'deviceScaleFactor' => 2.625,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const PIXEL_2_LANDSCAPE = [
+        'width' => 731,
+        'height' => 411,
+        'deviceScaleFactor' => 2.625,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const PIXEL_2_XL = [
+        'width' => 411,
+        'height' => 823,
+        'deviceScaleFactor' => 3.5,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => false,
+    ];
+    const PIXEL_2_XL_LANDSCAPE = [
+        'width' => 823,
+        'height' => 411,
+        'deviceScaleFactor' => 3.5,
+        'isMobile' => true,
+        'hasTouch' => true,
+        'isLandscape' => true,
+    ];
+
+}
